a434d4254c1c2eb70007d11f608b34fb380340ea
Juwel Rana <juwel.jem@gmail.com>
2024-12-24 05:04:49 +0600

    issue fixed

--- index.js
+++ index.js
@@ -40,12 +40,18 @@ async function run() {
       res.send(result);
     });
 
+    // get single data
+    app.get("/tutor/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await tutorials.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+
     // get data by category
     app.get("/tutorials/:category", async (req, res) => {
       const category = req.params.category;
       const result = await tutorials.find({ category: category }).toArray();
       res.send(result);
-      console.log(category);
     });
   } finally {
   }
